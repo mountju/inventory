ab5e6209f53ddc9bfe8de38a64d2529c466e84d7
Julia Melezhik <j.melezhik@mobidev.biz>
2018-01-13 17:36:00 +0200

    second commit

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import {
+    Component,
+    EventEmitter,
+    Input,
+    Output
+} from '@angular/core';
+
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+    @Input() productList: Product[];
+    @Output() onProductSelected: EventEmitter<Product>;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
